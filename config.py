31de4f35eb82f7a404be164d913c65d6cc0b6d2c
Jyneda <jyneda@gmail.com>
2021-07-08 17:41:26 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,17 @@
+
+#IoTtalk information
+ServerURL = 'IoTtalk Server URL' 
+device_name = 'Assing a name for WatchDog'
+polling_cycle = 600 # unit: second. ODF polling cycle (How long an ODF will be pulled)
+
+
+#WatchDog information
+inspection_cycle = 1800 # unit: second. How long WatchDog will check the ODF timestamp
+expiry_time = 600 # unit: second. Longer than expiry_time, sends LINE notification
+log_path = r'the dictionary storing log'
+
+
+#LINE notify token
+token_key = 'Put LINE notify token here'
+
+
