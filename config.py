08ef2e261b21a2838ba1c3ab991ab1c35f2c846d
彭星樺 <ktpss97094.cs09@m365.nycu.edu.tw>
2023-01-29 06:11:15 +0000

    修改config.py

--- config.py
+++ config.py
@@ -1,17 +1,18 @@
 
-#IoTtalk information
-ServerURL = 'IoTtalk Server URL' 
-device_name = 'Assing a name for WatchDog'
-polling_cycle = 600 # unit: second. ODF polling cycle (How long an ODF will be pulled)
+# IoTtalk information
+ServerURL = 'https://farm.iottalk.tw'
+device_name = 'NanZhuang'
+# unit: second. ODF polling cycle (How long an ODF will be pulled)(每次要詢問是否有新訊息的間隔時間)
+polling_cycle = 600
 
 
-#WatchDog information
-inspection_cycle = 1800 # unit: second. How long WatchDog will check the ODF timestamp
-expiry_time = 1200 # unit: second. Longer than expiry_time, sends LINE notification. Note: There are frequent false alerts if expiry_time <= polling_cycle.
-log_path = r'the dictionary storing log'
+# WatchDog information
+# unit: second. How long WatchDog will check the ODF timestamp (每此時間檢查訊息是否有在expiry_time內收進來)
+inspection_cycle = 1800
+# unit: second. Longer than expiry_time, sends LINE notification. Note: There are frequent false alerts if expiry_time <= polling_cycle.
+expiry_time = 1800
+log_path = r'/home/user1/WatchDog/log'  # log資料夾的路徑
 
 
-#LINE notify token
-token_key = 'Put LINE notify token here'
-
-
+# LINE notify token (在LINE notify網頁註冊的權杖)
+token_key = 'eOUh7Jm8IeEHqLzWcLz8ezBNPBgsbRBtk6zrl77qXbn'
